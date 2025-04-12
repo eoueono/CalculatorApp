e1cf4d8909858ffd1778146fbd75295257726777
eoueono <seoyeong524@koreatech.ac.kr>
2025-04-12 13:25:30 +0900

    Initial Commit

---
+++ CalculatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by 전서영 on 4/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CalculatorAppTests/CalculatorAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppTests.swift
+//  CalculatorAppTests
+//
+//  Created by 전서영 on 4/12/25.
+//
+
+import Testing
+@testable import CalculatorApp
+
+struct CalculatorAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
