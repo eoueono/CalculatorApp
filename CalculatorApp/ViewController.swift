c6a2d3adb72fd0a46fde2bec6ab033801acd944c
eoueono <seoyeong524@koreatech.ac.kr>
2025-04-15 03:11:44 +0900

    결과 출력 함수 구현

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let resultLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        setupResultLabel()
+        setupCalculatorButtons()
         // Do any additional setup after loading the view.
     }
 
+    func setupResultLabel() {
+        resultLabel.text = "0"
+            resultLabel.font = UIFont.systemFont(ofSize: 40)
+            resultLabel.textAlignment = .right
+            resultLabel.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(resultLabel)
 
+            NSLayoutConstraint.activate([
+                resultLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+                resultLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+                resultLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+                resultLabel.heightAnchor.constraint(equalToConstant: 80)
+            ])
+    }
+
+    func setupCalculatorButtons() {
+        
+    }
+    
 }
 
